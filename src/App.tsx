c48afc7de153f87a9486c9063895a151f5d24709
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-01-08 22:28:00 +0530

    feat : Added left array by d element brute approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,37 @@
 import "./App.css";
-import { binarySearch } from "./algo/binarySearch";
-import { bubble_sort } from "./algo/bubbleSort";
-import { frequencyCounter } from "./algo/freqeuncyCounter";
-import { insertionSort } from "./algo/insertionSort";
-import { lengthOfLongestSubstring } from "./algo/lengthOfLongestSubstring";
-import { maxSubArraySum } from "./algo/maxSubArraySum";
-import { merge, mergeSort } from "./algo/mergeSort";
-import { naiveSearch } from "./algo/naiveSearch";
-import { quickSort } from "./algo/quickSort";
 import { radixSort } from "./algo/radixSort";
 import { recursiveBubbleSort } from "./algo/recursiveBubbleSort";
-import { selection_sort } from "./algo/selectionSort";
-import { removeDuplicate } from "./leetcode/removeDuplicate";
-import { secondLargestElement } from "./leetcode/secondLargestElement";
+import { leftRotate, rotateLeftByDElement } from "./leetcode/leftRotate";
 
 function App() {
   const sortedArray = [1, 2, 3, 4, 5, 6, 7];
   const unsortedArray = [12, 7, 2, 19, 33, 17, 28, 5];
-  selection_sort(unsortedArray);
-  insertionSort(unsortedArray);
+  // selection_sort(unsortedArray);
+  // insertionSort(unsortedArray);
 
-  console.log("Bubble Sort", bubble_sort(unsortedArray));
-  console.log(frequencyCounter([1, 2, 3], [1, 4, 9]));
-  console.log(maxSubArraySum(unsortedArray, 1));
-  console.log(binarySearch([1, 2, 3, 5, 6, 9, 15, 19], 32));
+  // console.log("Bubble Sort", bubble_sort(unsortedArray));
+  // console.log(frequencyCounter([1, 2, 3], [1, 4, 9]));
+  // console.log(maxSubArraySum(unsortedArray, 1));
+  // console.log(binarySearch([1, 2, 3, 5, 6, 9, 15, 19], 32));
 
-  lengthOfLongestSubstring("abcdbaacvbcc");
-  console.log(naiveSearch("abclolabc", "abc"));
-  merge([1, 2, 8, 20], [28, 56, 59, 99]);
-  mergeSort(unsortedArray);
-  quickSort(unsortedArray);
-  radixSort([18, 232, 4, 332, 23425, 5232, 52321, 423, 421, 3234, 13]);
-  console.log(recursiveBubbleSort(unsortedArray));
-  console.log(
-    secondLargestElement(
-      radixSort([18, 232, 4, 332, 23425, 5232, 52321, 423, 421, 3234, 13])
-    )
-  );
+  // lengthOfLongestSubstring("abcdbaacvbcc");
+  // console.log(naiveSearch("abclolabc", "abc"));
+  // merge([1, 2, 8, 20], [28, 56, 59, 99]);
+  // mergeSort(unsortedArray);
+  // quickSort(unsortedArray);
+  // radixSort([18, 232, 4, 332, 23425, 5232, 52321, 423, 421, 3234, 13]);
+  // console.log(recursiveBubbleSort(unsortedArray));
+  // console.log(
+  //   secondLargestElement(
+  //     radixSort([18, 232, 4, 332, 23425, 5232, 52321, 423, 421, 3234, 13])
+  //   )
+  // );
 
-  removeDuplicate([1, 1, 2, 2, 3, 4, 4, 5, 5]);
+  // removeDuplicate([1, 1, 2, 2, 3, 4, 4, 5, 5]);
+
+  leftRotate([1, 2, 3, 4, 5]);
+  rotateLeftByDElement(unsortedArray, 3);
+  console.log(unsortedArray);
   return (
     <>
       <h1>Vite + React</h1>
