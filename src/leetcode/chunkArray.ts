73279eacefeb3ce71d69bf892bac6b1100c25c0a
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-01-15 11:02:27 +0530

    chore : chunk array

--- src/leetcode/chunkArray.ts
+++ src/leetcode/chunkArray.ts
@@ -2,9 +2,9 @@ type ChunkedArray = Array<number[]>;
 export const chunkArray = (nums: number[], chunkSize: number): ChunkedArray => {
   if (nums.length === 0) return [];
 
-  const chunk = nums.slice(0, chunkSize);
-
-  return [chunk, ...chunkArray(nums.slice(chunkSize), chunkSize)];
+  const firstChunk = nums.slice(0, chunkSize);
+  const remainingChunks = chunkArray(nums.slice(chunkSize), chunkSize);
+  return [firstChunk, ...remainingChunks];
 };
 
 // [1,9,6,3,2]
