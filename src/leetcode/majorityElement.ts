6010b39aa27c1301305053a3e776914d5810a274
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-02-19 22:37:23 +0530

    chore : with Map functions

--- src/leetcode/majorityElement.ts
+++ src/leetcode/majorityElement.ts
@@ -17,3 +17,17 @@ export const majorityElements = (nums: number[]): number => {
   }
   return 0;
 };
+
+const majorityElementsWithMap = (nums: number[]): number => {
+  const numCountMap = new Map<number, number>();
+
+  for (const num of nums) {
+    numCountMap.set(num, (numCountMap.get(num) || 0) + 1);
+
+    if (numCountMap.get(num)! > nums.length / 2) {
+      return num;
+    }
+  }
+  console.log("COUNT MAP", numCountMap);
+  return 0;
+};
