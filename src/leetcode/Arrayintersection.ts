f82b52ba666e115e7553ce12f0cd711c711758d9
undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-03-10 16:31:58 +0530

    feat : approach 3 for question no 349

--- src/leetcode/Arrayintersection.ts
+++ src/leetcode/Arrayintersection.ts
@@ -15,45 +15,45 @@
   return results;
 } */
 
-import { binarySearch } from "~/algo/binarySearch";
-
 /* 
 Approach 2
 */
 
+// export function arraysIntersection(nums1: number[], nums2: number[]) {
+//   const results: number[] = [];
+
+//   for (let i = 0; i < nums2.length; i++) {
+//     if (binarySearch(nums1, nums2[i])) {
+//       results.push(nums2[i]);
+//     }
+//   }
+
+//   return Array.from(new Set(results));
+// }
+
 export function arraysIntersection(nums1: number[], nums2: number[]) {
   const results: number[] = [];
 
-  for (let i = 0; i < nums2.length; i++) {
-    if (binarySearch(nums1, nums2[i])) {
-      results.push(nums2[i]);
+  /* First we will sort both of arrays */
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+  let i = 0,
+    j = 0;
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums1[i]) {
+      results.push(nums2[j]);
+      while (i < nums1.length - 1 && nums1[i] === nums1[i + 1]) i++;
+      while (j < nums2.length - 1 && nums2[j] === nums2[j + 1]) j++;
+      i++;
+      j++;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      j++;
     }
   }
+  console.log("RESULTS", results);
 
-  return Array.from(new Set(results));
+  return results;
 }
-
-// export function arraysIntersection(nums1: number[], nums2: number[]) {
-//   const results: number[] = [];
-//   let left = 0,
-//     right = 0;
-
-//   /* First we will sort both of arrays */
-//   nums1.sort((a, b) => a - b);
-//   nums2.sort((a, b) => a - b);
-
-//   while (left < nums1.length && right < nums2.length) {
-//     if (nums1[left] === nums1[left]) {
-//       results.push(nums2[right]);
-//       left++;
-//       right++;
-//     } else if (nums1[left] < nums2[right]) {
-//       left++;
-//     } else {
-//       right++;
-//     }
-//   }
-//   console.log("RESULTS", results);
-
-//   return results;
-// }
